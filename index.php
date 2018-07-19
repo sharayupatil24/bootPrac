793ee436290e642ab1d0867ff7b3fee1f5fb6b14
Sharayu Patil <2015sharayu.patil@ves.ac.in>
2018-07-19 16:17:15 +0530

    Navbar
    This is the simple navbar template commonly used for websites ..

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>HOPE</title>
+		<link rel="shortcut icon" type="image/x-icon" href="images/shortcut.png">
+		<link href="css/css.css" rel="stylesheet" type="text/css">
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/font-awesome.min.css" rel="stylesheet">
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<link href="css/style_1.css" rel="stylesheet">
+		<style type="text/css">
+
+				.home{
+					background-size: cover;
+				}
+
+			  .st-navbar-mini .navbar-default .navbar-nav > li > a {
+			    padding-top: 20px;
+			    padding-bottom: 20px;
+			    background: #323232;
+			    color: #fff;
+			  }
+			}
+		</style>
+	</head>
+	<body data-spy="scroll" data-target=".main-nav">
+
+		<header class="st-navbar">
+			<nav class="navbar navbar-default navbar-fixed-top clearfix" role="navigation">
+				<div class="container"><!-- Brand and toggle get grouped for better mobile display -->
+
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sept-main-nav">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse main-nav" id="sept-main-nav">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="active"><a href="#home">Home</a></li>
+							<li><a href="#about">Blog</a></li>
+							<li><a href="#service">Services</a></li>
+							<li><a href="#portfolio">About</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Section4<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#section 4.1">Section 4.1</a></li>
+									<li><a href="#section 4.2">Section 4.2</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+
+				</div>
+			</nav>
+		</header>
+
+
+    <section class="contact" id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+
+						<div class="section-title st-center">
+							<h3>HEADING</h3>
+							<p>Sub heading</p>
+						</div>
+
+            <div>
+              <p>
+                Lorem Ipsum is simply dummy text of the
+								printing and typesetting industry. Lorem Ipsum has
+								been the industry's standard dummy text ever since
+								the 1500s, when an unknown printer took a galley of type
+								 and scrambled it to make a type specimen book.
+								  It has survived not only five centuries, but also the
+									leap into electronic typesetting, remaining essentially
+									unchanged. It was popularised in the 1960s with the
+									release of Letraset sheets containing Lorem Ipsum passages,
+									 and more recently with desktop publishing software like
+									 Aldus PageMaker including versions of Lorem Ipsum.
+									 Lorem Ipsum is simply dummy text of the
+									printing and typesetting industry. Lorem Ipsum has
+									been the industry's standard dummy text ever since
+									the 1500s, when an unknown printer took a galley of type
+									 and scrambled it to make a type specimen book.
+										It has survived not only five centuries, but also the
+										leap into electronic typesetting, remaining essentially
+										unchanged. It was popularised in the 1960s with the
+										release of Letraset sheets containing Lorem Ipsum passages,
+										Lorem Ipsum is simply dummy text of the
+										printing and typesetting industry. Lorem Ipsum has
+										been the industry's standard dummy text ever since
+										the 1500s, when an unknown printer took a galley of type
+										 and scrambled it to make a type specimen book.
+										  It has survived not only five centuries, but also the
+											leap into electronic typesetting, remaining essentially
+											unchanged. It was popularised in the 1960s with the
+											release of Letraset sheets containing Lorem Ipsum passages,
+											Lorem Ipsum is simply dummy text of the
+											printing and typesetting industry. Lorem Ipsum has
+											been the industry's standard dummy text ever since
+											the 1500s, when an unknown printer took a galley of type
+											 and scrambled it to make a type specimen book.
+											  It has survived not only five centuries, but also the
+												leap into electronic typesetting, remaining essentially
+												unchanged. It was popularised in the 1960s with the
+												release of Letraset sheets containing Lorem Ipsum passages,
+
+              </p>
+            </div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.stellar.js"></script>
+<script src="js/jquery.appear.js"></script>
+<script src="js/jquery.nicescroll.min.js"></script>
+<script src="js/script.js"></script>
+<script src="js/myscript.js"></script>
+</body>
+</html>
